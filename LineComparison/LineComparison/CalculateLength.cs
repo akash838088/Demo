a7555167f812e0bfd8cf3e168601295090e3d8fd
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-06 00:52:20 +0530

    [Akash] add : UC1

---
+++ LineComparison/LineComparison/CalculateLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class CalculateLength
+    {
+        int lengthX,_lengthY,lenGthsX,lengthsY;
+        public CalculateLength(int lengthX,int lengthY,int lengthsX,int lengthsY)
+        {
+           this.lengthX = lengthX;
+            _lengthY=lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            //Line = sqrt (x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX),2) + Math.Pow((lengthsY - _lengthY),2));
+            Console.WriteLine(length);
+            return length;
+        }
+    }
+}
---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LineComparison
+{
+
+    public class Program
+    {
+
+        static void Main(string[]args)
+         {
+            CalculateLength length_One = new CalculateLength(1,2,3,4);
+            double length = length_One.Calculate();
+
+            CalculateLength length_Two = new CalculateLength(8,3,8,0);
+            double length_ = length_Two.Calculate();
+            
+
+            //UC2
+            if (length.Equals(length_))
+                Console.WriteLine("Both line are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
+
+            //UC3
+            if (length.CompareTo(length_) == 0)
+                Console.WriteLine("Equals");
+            else if (length.CompareTo(length_) > 0)
+                Console.WriteLine("Line One is Greater");
+            else
+                Console.WriteLine("Line Two is Greater");
+            Console.ReadLine();
+            
+        }
+    }
+}
